c86b122d7718ca7cd9fccf4c4f24c19adb32726c
nmorriss7109 <nmorriss7109@gmail.com>
2020-01-12 07:01:53 -0800

    ITS ALIIIIIVE

--- server/index.js
+++ server/index.js
@@ -1,18 +1,98 @@
 const express = require('express');
-const app = express();
-const Joi = require('joi');
+const server = express();
+//const Joi = require('joi');
 const cors = require('cors');
+const body_parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 
-app.use(express.json());
-app.use(cors());
+server.use(express.json());
+server.use(cors());
+server.use(body_parser.json());
 
-const items = [
-  {id: 1, foodID: 'banana', expiryDate: 10},
-  {id: 2, foodID: 'apple', expiryDate: 20},
-  {id: 3, foodID: 'milk', expiryDate: 5}
-];
+// const items = [
+//   {id: 1, foodID: 'banana', expiryDate: 10},
+//   {id: 2, foodID: 'apple', expiryDate: 20},
+//   {id: 3, foodID: 'milk', expiryDate: 5}
+// ];
 
+// << db setup >>
+const db = require("./db.js");
+const dbName = "data";
+const collectionName = "movies";
 
+// << db init >>
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+    // get all items
+    dbCollection.find().toArray(function(err, result) {
+        if (err) throw err;
+          console.log(result);
+    });
+
+    // << db CRUD routes >>
+    server.post("/api/items", (request, response) => {
+       const item = request.body;
+       dbCollection.insertOne(item, (error, result) => { // callback of insertOne
+          if (error) throw error;
+          // return updated list
+          dbCollection.find().toArray((_error, _result) => { // callback of find
+             if (_error) throw _error;
+             response.json(_result);
+          });
+       });
+     });
+
+    server.get("/api/items/:id", (request, response) => {
+       const itemId = request.params.id;
+
+       dbCollection.findOne({ id: itemId }, (error, result) => {
+          if (error) throw error;
+          // return item
+          response.json(result);
+       });
+    });
+
+    server.get("/api/items", (request, response) => {
+       // return updated list
+       dbCollection.find().toArray((error, result) => {
+          if (error) throw error;
+          response.json(result);
+       });
+    });
+
+    server.put("/api/items/:id", (request, response) => {
+       const itemId = request.params.id;
+       const item = request.body;
+       console.log("Editing item: ", itemId, " to be ", item);
+
+       dbCollection.updateOne({ id: itemId }, { $set: item }, (error, result) => {
+          if (error) throw error;
+          // send back entire updated list, to make sure frontend data is up-to-date
+          dbCollection.find().toArray(function (_error, _result) {
+             if (_error) throw _error;
+             response.json(_result);
+          });
+       });
+    });
+
+    server.delete("/api/items/:id", (request, response) => {
+       const itemId = request.params.id;
+       console.log("Delete item with id: ", itemId);
+
+       dbCollection.deleteOne({ id: itemId }, function (error, result) {
+          if (error) throw error;
+          // send back entire updated list after successful request
+          dbCollection.find().toArray(function (_error, _result) {
+             if (_error) throw _error;
+             response.json(_result);
+          });
+       });
+    });
+
+}, function(err) { // failureCallback
+    throw (err);
+});
+/*
 app.get('/', (req, res) => {
   res.send('Home!');
 });
@@ -86,10 +166,10 @@ function validateItem (body) {
   const result = Joi.validate(body, schema);
   return result;
 }
-
+*/
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => {console.log(`Listening on port ${port}...`)});
+server.listen(port, () => {console.log(`Listening on port ${port}...`)});
 
 
 
